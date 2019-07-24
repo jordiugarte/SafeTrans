9ecb7488886c11d533a54b319152735f09687776
jordiugarte <jordi933@gmail.com>
2019-07-23 20:01:44 -0400

    tools and login activity

--- app/src/main/java/com/digistring/safetrans/LoginActivity.java
+++ app/src/main/java/com/digistring/safetrans/LoginActivity.java
@@ -1,8 +1,14 @@
 package com.digistring.safetrans;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.digistring.safetrans.tools.Notification;
 
 import com.digistring.safetrans.dataBase.ConexionSQLiteHelper;
 
@@ -10,20 +16,32 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextView userField;
     private TextView passwordField;
+    private Button loginButon;
 
+    private Context context;
     private ConexionSQLiteHelper sqLiteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        context = getApplicationContext();
         setListeners();
 
-
+        loginButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "adasdas", Toast.LENGTH_SHORT);
+                if (userField.toString() == "" && passwordField.toString() == "") {
+                    new Notification("Ingrese datos válidos", context);
+                }
+            }
+        });
     }
 
     private void setListeners() {
         userField = findViewById(R.id.userField);
         passwordField = findViewById(R.id.passwordField);
+        loginButon = findViewById(R.id.loginButton);
     }
 }
